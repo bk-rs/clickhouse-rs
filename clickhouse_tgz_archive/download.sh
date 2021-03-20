aad22c02a58d21f83d7678a63da9d419da0ec11e
vkill <vkill.net@gmail.com>
2021-03-20 21:29:50 +0800

    Support download clickhouse-client.tgz

--- clickhouse_tgz_archive/download.sh
+++ clickhouse_tgz_archive/download.sh
@@ -12,11 +12,14 @@ script_path_root="${script_path}/"
 
 rm -rf "${script_path_root}clickhouse-common-static-${version}.tgz"
 rm -rf "${script_path_root}clickhouse-server-${version}.tgz"
+rm -rf "${script_path_root}clickhouse-client-${version}.tgz"
 wget -O "${script_path_root}clickhouse-common-static-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-common-static-${version}.tgz"
 wget -O "${script_path_root}clickhouse-server-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-server-${version}.tgz"
+wget -O "${script_path_root}clickhouse-client-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-client-${version}.tgz"
 
 tgz_root_path="${script_path_root}clickhouse"
 rm -rf "${tgz_root_path}"
 mkdir "${tgz_root_path}"
 tar -zxvf "${script_path_root}clickhouse-common-static-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
 tar -zxvf "${script_path_root}clickhouse-server-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
+tar -zxvf "${script_path_root}clickhouse-client-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
