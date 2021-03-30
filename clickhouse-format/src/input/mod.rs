2faaa44579c184167a340fbdf0aafa705fec929c
vkill <vkill.net@gmail.com>
2021-03-30 11:08:16 +0800

    Tweak

--- clickhouse-format/src/input/mod.rs
+++ clickhouse-format/src/input/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "with-json")]
 pub mod json_compact_each_row;
 
+#[cfg(feature = "with-json")]
+pub use self::json_compact_each_row::JSONCompactEachRowInput;
+
 pub trait Input {
     type Error;
 
