4e362e64b7615cd3f04a8686cf9825ca99a31669
vkill <vkill.net@gmail.com>
2021-04-06 01:35:13 +0800

    Remove features from clickhouse-data-value

--- clickhouse-data-value/src/lib.rs
+++ clickhouse-data-value/src/lib.rs
@@ -1,13 +1,8 @@
-#[cfg(any(feature = "with-datetime", feature = "with-datetime64"))]
 pub(crate) mod date_and_time_parser;
 
-#[cfg(feature = "with-date")]
 pub mod date;
-#[cfg(feature = "with-datetime")]
 pub mod datetime;
-#[cfg(feature = "with-datetime64")]
 pub mod datetime64;
 
-#[cfg(any(feature = "with-datetime", feature = "with-datetime64"))]
 // 2105-12-31 23:59:59
 pub(crate) const MAX_DATETIME_UNIX_TIMESTAMP: u64 = 4291718399;
