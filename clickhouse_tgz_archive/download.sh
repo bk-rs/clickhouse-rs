88a64f4a4e51416e90fba68020f1d856633cd636
vkill <vkill.net@gmail.com>
2023-02-17 10:16:22 +0800

    Update clickhouse_tgz_archive

--- clickhouse_tgz_archive/download.sh
+++ clickhouse_tgz_archive/download.sh
@@ -2,24 +2,24 @@
 
 set -ex
 
-# ./download.sh v21.4.1.6378-testing
+# ./download.sh v23.1.3.5-stable
 
-tag="${1:-v21.2.7.11-stable}"
+tag="${1:-v23.1.3.5-stable}"
 version=`echo $tag | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'`
 
 script_path=$(cd $(dirname $0) ; pwd -P)
 script_path_root="${script_path}/"
 
-rm -rf "${script_path_root}clickhouse-common-static-${version}.tgz"
-rm -rf "${script_path_root}clickhouse-server-${version}.tgz"
-rm -rf "${script_path_root}clickhouse-client-${version}.tgz"
-wget -O "${script_path_root}clickhouse-common-static-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-common-static-${version}.tgz"
-wget -O "${script_path_root}clickhouse-server-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-server-${version}.tgz"
-wget -O "${script_path_root}clickhouse-client-${version}.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-client-${version}.tgz"
+rm -rf "${script_path_root}clickhouse-common-static-${version}-amd64.tgz"
+rm -rf "${script_path_root}clickhouse-server-${version}-amd64.tgz"
+rm -rf "${script_path_root}clickhouse-client-${version}-amd64.tgz"
+wget -O "${script_path_root}clickhouse-common-static-${version}-amd64.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-common-static-${version}-amd64.tgz"
+wget -O "${script_path_root}clickhouse-server-${version}-amd64.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-server-${version}-amd64.tgz"
+wget -O "${script_path_root}clickhouse-client-${version}-amd64.tgz" "https://github.com/ClickHouse/ClickHouse/releases/download/${tag}/clickhouse-client-${version}-amd64.tgz"
 
 tgz_root_path="${script_path_root}clickhouse"
 rm -rf "${tgz_root_path}"
 mkdir "${tgz_root_path}"
-tar -zxvf "${script_path_root}clickhouse-common-static-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
-tar -zxvf "${script_path_root}clickhouse-server-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
-tar -zxvf "${script_path_root}clickhouse-client-${version}.tgz" -C "${tgz_root_path}" --strip-components=2
+tar -zxvf "${script_path_root}clickhouse-common-static-${version}-amd64.tgz" -C "${tgz_root_path}" --strip-components=2
+tar -zxvf "${script_path_root}clickhouse-server-${version}-amd64.tgz" -C "${tgz_root_path}" --strip-components=2
+tar -zxvf "${script_path_root}clickhouse-client-${version}-amd64.tgz" -C "${tgz_root_path}" --strip-components=2
